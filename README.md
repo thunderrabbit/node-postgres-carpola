b388d812b66f2173868a8589a6cfebdc2ba38d64
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-16 00:54:20 +0000

    First version, from
    https://codeforgeek.com/node-js-tutorial-step-by-step/

---
+++ README.md
@@ -0,0 +1,6 @@
+## One step along the way helped me play with Postgres on node
+
+
+from https://codeforgeek.com/node-js-tutorial-step-by-step/
+
+Used in https://github.com/thunderrabbit/learn-me-a-node
---
+++ app.js
@@ -0,0 +1,45 @@
+// from  https://codeforgeek.com/node-js-tutorial-step-by-step/
+
+const Pool = require("pg").Pool;
+const pool = new Pool({
+ user: "godzilla",
+ host: "localhost",
+ database: "godzilla",
+ password: "godzilla",
+ port: 5432,
+});
+
+// read information from table
+pool.query("SELECT * FROM profile ORDER BY id ASC", (error, results) => {
+ if (error) {
+   console.log(error);
+   return;
+ }
+ console.log(results);
+});
+
+// add a new user profile.
+const name = "Hello";
+const email = "hello@robnugen.com";
+
+pool.query(
+ "INSERT INTO profile (name, email) VALUES ($1, $2)",
+ [name, email],
+ (error, results) => {
+   if (error) {
+     console.log(error);
+     return;
+   }
+   console.log(results);
+ }
+);
+
+// read information from table with new row in it
+pool.query("SELECT * FROM profile ORDER BY id ASC", (error, results) => {
+ if (error) {
+   console.log(error);
+   return;
+ }
+ console.log(results);
+});
+
